4640f03860eab44899fbe8f335ee4884d7bb5811
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-09-26 12:16:30 +0530

    Update Fitness.c

--- Fitness.c
+++ Fitness.c
@@ -1,3 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct node {
+    int data;
+    struct node* east;
+    struct node* west;
+    struct node* north;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->east = newNode->west = newNode->north = NULL;
+    return newNode;
+}
+
+// Function to dynamically fill the linked list with random data
+void fillLinkedList(Node** head, int size, int min, int max) {
+    srand(time(NULL)); // seed random number generator
+    Node* current = *head;
+    for (int i = 0; i < size; i++) {
+        int data = min + rand() % (max - min + 1); // generate random data within the range [min, max]
+        Node* newNode = createNode(data);
+        if (current == NULL) {
+            *head = newNode;
+            current = *head;
+        } else {
+            current->east = newNode;
+            current = current->east;
+        }
+    }
+}
+
+// Function to print the linked list
+void printLinkedList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d---> ", current->data);
+        current = current->east;
+    }
+    printf("NULL\n");
+}
 
 // Fitness function for GWO
 double fitness(Node* wolf, int target) {
@@ -102,4 +146,23 @@ void gwoSearch(Node* head, int target) {
     }
 
     printf("Target %d not found after %d iterations.\n", target, max_iterations);
+}
+
+int main() {
+    int size, min, max;
+    printf("Enter the number of data items to generate: ");
+    scanf("%d", &size);
+    printf("Enter the minimum value of the range: ");
+    scanf("%d", &min);
+    printf("Enter the maximum value of the range: ");
+    scanf("%d", &max);
+
+    Node* head = NULL;
+    fillLinkedList(&head, size, min, max); // fill the linked list with random nodes within the specified range
+    printLinkedList(head); // print the linked list
+
+    int target = 42; // target value to search for
+    gwoSearch(head, target); // perform GWO search
+
+    return 0;
 }
