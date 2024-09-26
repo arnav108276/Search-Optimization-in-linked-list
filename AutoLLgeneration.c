9d3c53c754bde29d8d2c822814a2a467d733c5ba
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-09-26 11:33:21 +0530

    Update AutoLLgeneration.c

--- AutoLLgeneration.c
+++ AutoLLgeneration.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node {   //Structure of Linked list
+#include <time.h>
+
+typedef struct node {
     int data;
-    struct node *east;
-    struct node *west;
-    struct node *north;
+    struct node* east;
+    struct node* west;
+    struct node* north;
 } Node;
-Node* createNode(int data) {                                                        
-    Node* newNode = (Node*) malloc(sizeof(Node));                                                      //dynamically memory allocation
+
+Node* createNode(int data) {
+    Node* newNode = (Node*) malloc(sizeof(Node));
     newNode->data = data;
     newNode->east = newNode->west = newNode->north = NULL;
     return newNode;
 }
 
 // Function to dynamically fill the linked list with random data
-void fillLinkedList(Node** head, int size) {
+void fillLinkedList(Node** head, int size, int min, int max) {
     srand(time(NULL)); // seed random number generator
     Node* current = *head;
     for (int i = 0; i < size; i++) {
-        int data = rand() % 100; // generate random data between 0 and 99
+        int data = min + rand() % (max - min + 1); 
         Node* newNode = createNode(data);
         if (current == NULL) {
             *head = newNode;
@@ -29,6 +32,7 @@ void fillLinkedList(Node** head, int size) {
         }
     }
 }
+
 // Function to print the linked list
 void printLinkedList(Node* head) {
     Node* current = head;
@@ -38,9 +42,18 @@ void printLinkedList(Node* head) {
     }
     printf("NULL\n");
 }
+
 int main() {
+    int size, min, max;
+    printf("Enter the number of data items to generate: ");
+    scanf("%d", &size);
+    printf("Enter the minimum value of the range: ");
+    scanf("%d", &min);
+    printf("Enter the maximum value of the range: ");
+    scanf("%d", &max);
+
     Node* head = NULL;
-    fillLinkedList(&head, 1000); // fill the linked list with 20 random nodes
+    fillLinkedList(&head, size, min, max); // fill the linked list with random nodes within the specified range
     printLinkedList(head); // print the linked list
     return 0;
 }
